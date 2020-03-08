cb18d02edf7f042354701d7b77ae5f842ba3d6f5
rabia.q123 <rqureshi@uoguelph.ca>
2020-03-08 07:19:44 -0400

    Day 4: completed Q2.1!

--- P21.c
+++ P21.c
@@ -10,45 +10,16 @@
 #include <time.h>
 #include <stdbool.h>
 
-// function prototype
-int findIndex(char[], char[]);
-
-
-/* find the index at which the occurence happens 
- * return the value of the index
- */
-/* int findIndex(char file[3296951], char pattern[100]) {
-    int fileLen = 3296951, patternLen = 0;
-    int numIterations = 0; // bufferLen - patternLen
-    int numCharsMatched = 0;
-
-    patternLen = strlen(pattern);  
-    numIterations = fileLen - patternLen;
-
-    for (int j = 0; j < numIterations; j++) {
-        numCharsMatched = 0;
-        while ((numCharsMatched < patternLen) && (pattern[numCharsMatched] = file[j + numCharsMatched])) {
-            numCharsMatched++;
-        }
-        // pattern found in text
-        if (numCharsMatched == patternLen) {
-            return j;
-        }
-    }
-
-    return -1;
-}
- */
-
-/* prompt for pattern and finds all the occurrences of the pattern in the text using brute force
+/* prompt for pattern and find all occurrences of the pattern in the text using brute force
  * display num occurrences found, num shifts required, and runtime
  */
-void stringSearchBruteForce(char *file) { // 3296951 chars in file
+void stringSearchBruteForce(char *file) {
     char pattern[100] = {'\0'};
     int fileLen = 0, patternLen = 0;
     int numIterations = 0; // fileLen - patternLen
-    int numCharsMatched = 0, index = 0; // location at which pattern is found, if found
+    int numCharsMatched = 0;
     int numMatches = 0;
+    int patternShift = 0;
 
     // for program execution time
 	clock_t start, finish;
@@ -60,31 +31,37 @@ void stringSearchBruteForce(char *file) { // 3296951 chars in file
     scanf(" %s", pattern);
     strtok(pattern, "\n"); // remove newline from pattern
 
+    // calculating for loop iterations
     patternLen = strlen(pattern);  
     fileLen = strlen(file);
     numIterations = fileLen - patternLen;
 
-//    printf("patternLen = %d, fileLen = %d, numIterations = %d\n", patternLen, fileLen, numIterations);
-
-    // display num anagrams found (if any)
-    printf("Index/indices at which pattern was found: \n");
+    // calclating runtime of search (start)
+    start = clock();
 
     // shifting pattern array by 1 each time
     for (int i = 0; i <= numIterations; i++) {
         for (numCharsMatched = 0; numCharsMatched < patternLen; numCharsMatched++) {
-            // checking for string match with given index i
+            // checking for string match with current index i
             if (file[i + numCharsMatched] != pattern[numCharsMatched]) {
+                patternShift++;
                 break; 
-            } 
+            }
         }
+        // string search successful
         if (numCharsMatched == patternLen) {
-            printf("%d \n", i); 
             numMatches++;
         }
     }
+    
+    // calculating runtime of search (complete!)
+    finish = clock();
+    searchTime = (double)(finish - start) / CLOCKS_PER_SEC;
 
     // printing stats
+    printf("Pattern shift: %d \n", patternShift); 
     printf("Number of matches found: %d\n", numMatches);
+    printf("Run time for search: %fs\n", searchTime);
 
     return;
 }
